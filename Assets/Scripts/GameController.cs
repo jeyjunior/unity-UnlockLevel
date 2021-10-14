52846c48716e5ec7f5bb29393637bebe128611f1
jeyjunior <jeyjuniordg@gmail.com>
2021-10-14 19:56:29 -0300

    Projeto Base

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+
+    public void WinBtn()
+    {
+
+        //Registramos (int) na key LevelComplete quando a cena é completada pela primeira vez
+        //Caso contrario, ignoramos o save e pulos para o else
+        if(PlayerPrefs.GetInt("LevelComplete") < SceneManager.GetActiveScene().buildIndex)
+        {
+            
+            PlayerPrefs.SetInt("LevelComplete",SceneManager.GetActiveScene().buildIndex);
+            LoadScene();
+        }
+        else
+        {
+            Debug.Log("Level completado");
+            Invoke("LoadScene", 2f);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/levelManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class levelManager : MonoBehaviour
+{
+
+    //public bool[] level;
+    public Button[] btnLevel;
+    public int btnActive = 0; //Valor dessa var é igual ao valor da fase carregada (fase1 = 1, fase2 = 2...)
+
+    private void Start()
+    {
+        //Recuperamos da key LevelComplete o valor (int) do total de fases liberadas
+        btnActive = PlayerPrefs.GetInt("LevelComplete");
+    }
+
+    private void Update()
+    {
+
+        //valor de i sempre é inicializado com o valor que esta na var btnActive
+        //esse valor de inicialização reflete em qual btn do array btnLevel, sera habilitado
+        //os botões habilitados representam as fases que foram concluidas
+        for (int i = btnActive; i < btnLevel.Length; i++) {
+
+            btnLevel[i].interactable = false;
+        }
+    }
+
+
+    //level select btn
+    public void LoadLevel(Button btnName)
+    {
+        SceneManager.LoadScene(btnName.name);
+    }
+
+    //
+    public void ZerarSaves()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
